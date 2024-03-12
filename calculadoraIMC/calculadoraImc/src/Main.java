2cb9a3105eecc3e0297bbd83f982c072716ef4c2
EduardoMRT <eduksoficial@gmail.com>
2024-03-12 12:18:51 -0300

    abc

--- calculadoraIMC/calculadoraImc/src/Main.java
+++ calculadoraIMC/calculadoraImc/src/Main.java
@@ -4,7 +4,7 @@ import domain.Pessoa;
 public class Main {
     public static void main(String[] args) {
         CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
-        Pessoa Eduardo = new Pessoa("Eduardo", 18, 55, 1.70);
+        Pessoa Eduardo = new Pessoa("Eduardo", 18, 90, 1.70);
         calculadoraIMC.calcularIMC(Eduardo);
 }
 }
